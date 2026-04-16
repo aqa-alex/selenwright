7f7391dcafafa3ba9257fd94f6c4f5a087f94059
Sasha <syshist@gmail.com>
2026-04-16 17:03:41 +0200

    Fix data race in MetadataProcessor.OnSessionStopped
    Snapshot app.logOutputDir under artifactHistoryMu before use,
    matching the lock discipline in playwright_test.go and
    artifact_history.go.

--- metadata.go
+++ metadata.go
@@ -26,34 +26,37 @@ type MetadataProcessor struct {
 }
 
 func (mp *MetadataProcessor) OnSessionStopped(stoppedSession event.StoppedSession) {
-	if app.logOutputDir != "" {
-		meta := session.Metadata{
-			ID:           stoppedSession.SessionId,
-			Started:      stoppedSession.Session.Started,
-			Finished:     time.Now(),
-			Capabilities: stoppedSession.Session.Caps,
-		}
-		data, err := json.MarshalIndent(meta, "", "    ")
-		if err != nil {
-			log.Printf("[%d] [METADATA] [%s] [Failed to marshal: %v]", stoppedSession.RequestId, stoppedSession.SessionId, err)
-			return
-		}
-		filename, err := safepath.Join(app.logOutputDir, stoppedSession.SessionId+metadataFileExtension)
-		if err != nil {
-			log.Printf("[%d] [METADATA] [%s] [Rejected session id: %v]", stoppedSession.RequestId, stoppedSession.SessionId, err)
-			return
-		}
-		err = os.WriteFile(filename, data, 0o644)
-		if err != nil {
-			log.Printf("[%d] [METADATA] [%s] [Failed to save to %s: %v]", stoppedSession.RequestId, stoppedSession.SessionId, filename, err)
-			return
-		}
-		log.Printf("[%d] [METADATA] [%s] [%s]", stoppedSession.RequestId, stoppedSession.SessionId, filename)
-		createdFile := event.CreatedFile{
-			Event: stoppedSession.Event,
-			Name:  filename,
-			Type:  "metadata",
-		}
-		event.FileCreated(createdFile)
+	artifactHistoryMu.Lock()
+	logDir := app.logOutputDir
+	artifactHistoryMu.Unlock()
+	if logDir == "" {
+		return
 	}
+	meta := session.Metadata{
+		ID:           stoppedSession.SessionId,
+		Started:      stoppedSession.Session.Started,
+		Finished:     time.Now(),
+		Capabilities: stoppedSession.Session.Caps,
+	}
+	data, err := json.MarshalIndent(meta, "", "    ")
+	if err != nil {
+		log.Printf("[%d] [METADATA] [%s] [Failed to marshal: %v]", stoppedSession.RequestId, stoppedSession.SessionId, err)
+		return
+	}
+	filename, err := safepath.Join(logDir, stoppedSession.SessionId+metadataFileExtension)
+	if err != nil {
+		log.Printf("[%d] [METADATA] [%s] [Rejected session id: %v]", stoppedSession.RequestId, stoppedSession.SessionId, err)
+		return
+	}
+	err = os.WriteFile(filename, data, 0o644)
+	if err != nil {
+		log.Printf("[%d] [METADATA] [%s] [Failed to save to %s: %v]", stoppedSession.RequestId, stoppedSession.SessionId, filename, err)
+		return
+	}
+	log.Printf("[%d] [METADATA] [%s] [%s]", stoppedSession.RequestId, stoppedSession.SessionId, filename)
+	event.FileCreated(event.CreatedFile{
+		Event: stoppedSession.Event,
+		Name:  filename,
+		Type:  "metadata",
+	})
 }
